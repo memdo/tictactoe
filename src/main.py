10a6a71a2941096626ce6be3176a4403385cb93c
M. Berkay Düzenli <memdo@pm.me>
2021-12-28 11:00:09 +0300

    Render the board

--- src/main.py
+++ src/main.py
@@ -3,3 +3,17 @@ def new_board():
             [None, None, None],
             [None, None, None]]
 
+def get_char(char):
+    if char == "X" or char == "O":
+        return char
+    else:
+        return " "
+
+def render(board):
+    print("  0 1 2")
+    print("  ------")
+
+    for i in range(3):
+        print(f"{i}|{get_char(board[i][0])} {get_char(board[i][1])} {get_char(board[i][2])}|")
+    print("  ------")
+
