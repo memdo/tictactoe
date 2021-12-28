7bec4fa29e8433fa7598640cf517511aa9516946
M. Berkay Düzenli <memdo@pm.me>
2021-12-28 16:35:50 +0300

    Determine the winner

--- src/main.py
+++ src/main.py
@@ -32,6 +32,20 @@ def make_move(board, coords, side):
         print("This square is not empty, try again.")
     return False
 
+def get_winner(board):
+    lines = [[board[0][0], board[0][1], board[0][2]],
+             [board[1][0], board[1][1], board[1][2]],
+             [board[2][0], board[2][1], board[2][2]],
+             [board[0][0], board[1][0], board[2][0]],
+             [board[0][1], board[1][1], board[2][1]],
+             [board[0][2], board[1][2], board[2][2]],
+             [board[0][0], board[1][1], board[2][2]],
+             [board[2][0], board[1][1], board[0][2]]]
+    for i in lines:
+        if bool(i[0]) and bool([i[1]]) and bool(i[2]):
+            if i[0] == i[1] == i[2]:
+                return i[0]
+
 board = new_board()
 side = "X"
 move_count = 0
@@ -42,7 +56,13 @@ while move_count < 9:
     if make_move(board, get_move(), side):
         side = "O" if side == "X" else "X"
         move_count += 1
+    if get_winner(board):
+        break
     print("----------------------------")
 
 render(board)
+if get_winner(board):
+    print("Winner:", get_winner(board))
+else:
+    print("Draw!")
 print("----------------------------")
